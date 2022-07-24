13ea82fe1672e3d5e8f7711bcf27351afd92ddc8
Yuri Panchul <yuri@panchul.com>
2022-07-23 23:49:23 -0700

    Minor change

--- scripts/extract_logs_from_openlane.bash
+++ scripts/extract_logs_from_openlane.bash
@@ -27,15 +27,16 @@ target_dir=$lecture_dir/$run_name
 ! [ -d $target_dir ] || error "\"$target_dir\" already exists"
 mkdir $target_dir || error "Cannot create \"$target_dir\""
 
-   cp $last_run_dir/results/synthesis/moore_fsm.v             $target_dir/01_synthesis.v
-&& cp $last_run_dir/results/placement/moore_fsm.resized.v     $target_dir/02_placement.v
-&& cp $last_run_dir/results/cts/moore_fsm.resized.v           $target_dir/03_cts.v
-&& cp $last_run_dir/results/routing/moore_fsm.resized.v       $target_dir/04_routing.v
-&& cp $last_run_dir/results/final/verilog/gl/moore_fsm.v      $target_dir/05_final.v
-&& cp $last_run_dir/reports/signoff/29-rcx_mca_sta.rpt        $target_dir/06_mca_sta.rpt
-&& cp $last_run_dir/reports/signoff/29-rcx_mca_sta.area.rpt   $target_dir/07_mca_area.rpt
-&& cp $last_run_dir/reports/signoff/29-rcx_mca_sta.power.rpt  $target_dir/08_mca_power.rpt
-&& cp $last_run_dir/results/signoff/moore_fsm.mag             $target_dir/09_magic.mag
+                                                                        # 01_main.log
+   cp $last_run_dir/results/synthesis/moore_fsm.v             $target_dir/02_synthesis.v
+&& cp $last_run_dir/results/placement/moore_fsm.resized.v     $target_dir/03_placement.v
+&& cp $last_run_dir/results/cts/moore_fsm.resized.v           $target_dir/04_cts.v
+&& cp $last_run_dir/results/routing/moore_fsm.resized.v       $target_dir/05_routing.v
+&& cp $last_run_dir/results/final/verilog/gl/moore_fsm.v      $target_dir/06_final.v
+&& cp $last_run_dir/reports/signoff/29-rcx_mca_sta.rpt        $target_dir/07_mca_sta.rpt
+&& cp $last_run_dir/reports/signoff/29-rcx_mca_sta.area.rpt   $target_dir/08_mca_area.rpt
+&& cp $last_run_dir/reports/signoff/29-rcx_mca_sta.power.rpt  $target_dir/09_mca_power.rpt
+&& cp $last_run_dir/results/signoff/moore_fsm.mag             $target_dir/10_magic.mag
 || error "Cannot copy something"
 
 exit 0
