625d2a247bb1c8046314820aeef308969fbe3032
Yuri Panchul <yuri@panchul.com>
2022-07-24 01:17:32 -0700

    Workaround bug in Icarus

--- day_3/lab_8_fsm/snail_moore_fsm.sv
+++ day_3/lab_8_fsm/snail_moore_fsm.sv
@@ -34,7 +34,14 @@ module snail_moore_fsm
         case (state)
         S0: if (~ a) next_state = S1;
         S1: if (  a) next_state = S2;
-        S2: next_state = a ? S0 : S1;
+
+        // S2: next_state = a ? S0 : S1;
+
+        S2: if (a)
+              next_state = S0;
+            else
+              next_state = S1;
+
         endcase
     end
 
