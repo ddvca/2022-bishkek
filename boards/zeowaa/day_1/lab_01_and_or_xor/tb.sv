8ed4fc8ed4584fcdbdaabeab14f0f40619c15bca
Yuri Panchul <yuri@panchul.com>
2022-09-17 01:05:33 -0700

    First ZEOWAA example should work

--- boards/zeowaa/day_1/lab_01_and_or_xor/tb.sv
+++ boards/zeowaa/day_1/lab_01_and_or_xor/tb.sv
@@ -1,16 +1,19 @@
 module tb;
 
-    logic [3:0] key_sw;
+    logic [3:0] key;
+    logic [7:0] sw;
 
-    top i_top (.key_sw (key_sw));
+    top i_top (.key (key), .sw (sw));
 
     initial
     begin
+        #0
+
         `ifdef __ICARUS__
             $dumpvars;
         `endif
 
-        repeat (8) #10 key_sw <= $random;
+        repeat (8) #10 { key, sw } <= $random;
 
         `ifdef MODEL_TECH  // Mentor ModelSim and Questa
             $stop;
--- boards/zeowaa/day_1/lab_01_and_or_xor/top.sv
+++ boards/zeowaa/day_1/lab_01_and_or_xor/top.sv
@@ -1,47 +1,48 @@
 module top
 (
-    input        clk,
-    input        reset_n,
-    
-    input  [3:0] key_sw,
-    output [3:0] led,
+    input         clk,
+    input  [ 3:0] key,
+    input  [ 7:0] sw,
+    output [11:0] led,
 
-    output [7:0] abcdefgh,
-    output [3:0] digit,
+    output [ 7:0] abcdefgh,
+    output [ 7:0] digit,
 
-    output       buzzer,
+    output        vsync,
+    output        hsync,
+    output [ 2:0] rgb,
 
-    output       hsync,
-    output       vsync,
-    output [2:0] rgb
+    inout  [18:0] gpio
 );
 
-    assign abcdefgh  = 8'hff;
-    assign digit     = 4'hf;
-    assign buzzer    = 1'b0;
-    assign hsync     = 1'b1;
-    assign vsync     = 1'b1;
-    assign rgb       = 3'b0;
+    assign abcdefgh = 8'hff;
+    assign digit    = 8'b0;
+    assign hsync    = 1'b1;
+    assign vsync    = 1'b1;
+    assign rgb      = 3'b0;
 
-    wire a = ~ key_sw [0];
-    wire b = ~ key_sw [1];
+    wire a = ~ key [0];
+    wire b = ~ key [1];
     
     wire result = a ^ b;
 
     assign led [0] = ~ result;
     
-    assign led [1] = ~ (~ key_sw [0] ^ ~ key_sw [1]);
+    assign led [1] = ~ (~ key [0] ^ ~ key [1]);
 
     // Exercise 1: Change the code below.
-    // Assign to led [2] the result of AND operation
+    // Write the same for AND and OR operations
     
-    assign led [2] = 1'b0;
+    assign led [2] = 1'b1;
+    assign led [3] = 1'b1;
 
     // Exercise 2: Change the code below.
-    // Assign to led [3] the result of XOR operation
+    // Assign to led [4] the result of XOR operation
     // without using "^" operation.
     // Use only operations "&", "|", "~" and parenthesis, "(" and ")".
 
-    assign led [3] = 1'b0;
+    assign led [4] = 1'b1;
+
+    assign led [11:5] = 7'h7f;
 
 endmodule
