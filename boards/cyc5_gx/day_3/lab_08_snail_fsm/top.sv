cad79cb5e40ca47bfc6a299ebbaab80e5681c68c
Yuri Panchul <yuri@panchul.com>
2023-06-10 11:48:28 -0700

    Adding Cyclone V GX support

--- boards/cyc5_gx/day_3/lab_08_snail_fsm/top.sv
+++ boards/cyc5_gx/day_3/lab_08_snail_fsm/top.sv
@@ -1,128 +1,79 @@
 module top
-#(
-    parameter debounce_depth             = 8,
-              shift_strobe_width         = 23,
-              seven_segment_strobe_width = 10
-)
 (
     input           clk,
+    input           rst_n,
 
-    input   [ 1:0]  key,
+    input   [ 3:0]  key,
     input   [ 9:0]  sw,
     output  [ 9:0]  led,
+    output  [ 7:0]  ledg,
 
     output  [ 7:0]  hex0,
     output  [ 7:0]  hex1,
     output  [ 7:0]  hex2,
     output  [ 7:0]  hex3,
-    output  [ 7:0]  hex4,
-    output  [ 7:0]  hex5,
-
-    output          vga_hs,
-    output          vga_vs,
-    output  [ 3:0]  vga_r,
-    output  [ 3:0]  vga_g,
-    output  [ 3:0]  vga_b,
 
     inout   [35:0]  gpio
 );
 
     //------------------------------------------------------------------------
 
-    wire reset = ~ key [0];
+    assign ledg = '0;
+
+    wire reset = ~ rst_n;
 
     //------------------------------------------------------------------------
 
-    wire [1:0] key_db;
-    wire [9:0] sw_db;
+    wire enable;
+    wire fsm_in, moore_fsm_out, mealy_fsm_out;
 
-    sync_and_debounce # (.w (2), .depth (debounce_depth))
-        i_sync_and_debounce_key
-            (clk, reset, ~ key, key_db);
+    wire [3:0] shift_reg_par_out;
+    assign led = ~ shift_reg_par_out;
 
-    sync_and_debounce # (.w (10), .depth (debounce_depth))
-        i_sync_and_debounce_sw
-            (clk, reset, sw, sw_db);
+    strobe_gen i_strobe_gen
+        (.strobe (enable), .*);
+
+    shift_reg # (.depth (4)) i_shift_reg
+    (
+        .en      ( enable            ),
+        .seq_in  ( ~& key            ),  // Same as key != 4'b1111
+        .seq_out ( fsm_in            ),
+        .par_out ( shift_reg_par_out ),
+        .*
+    );
+
+    snail_moore_fsm i_moore_fsm
+        (.en (enable), .a (fsm_in), .y (moore_fsm_out), .*);
+
+    snail_mealy_fsm i_mealy_fsm
+        (.en (enable), .a (fsm_in), .y (mealy_fsm_out), .*);
 
     //------------------------------------------------------------------------
 
-    wire shift_strobe;
+    //   --a--
+    //  |     |
+    //  f     b
+    //  |     |
+    //   --g--
+    //  |     |
+    //  e     c
+    //  |     |
+    //   --d--  h
+    //
+    //  0 means light
 
-    strobe_gen # (.w (shift_strobe_width)) i_shift_strobe
-        (clk, reset, shift_strobe);
+    always_comb
+      case ({ mealy_fsm_out, moore_fsm_out })
+      2'b00: abcdefgh = 8'b1111_1111;
+      2'b01: abcdefgh = 8'b0011_1001;  // Moore only
+      2'b10: abcdefgh = 8'b1100_0101;  // Mealy only
+      2'b11: abcdefgh = 8'b0000_0001;
+      endcase
 
-    wire [9:0] out_reg;
+    assign digit = 4'b1110;
 
-    shift_register # (.w (10)) i_shift_reg
-    (
-        .clk     ( clk          ),
-        .reset   ( reset        ),
-        .en      ( shift_strobe ),
-        .in      ( key_db [1]   ),
-        .out_reg ( out_reg      )
-    );
-
-    assign led = out_reg;
-
-    //------------------------------------------------------------------------
-
-    wire [15:0] shift_strobe_count;
-
-    counter # (16) i_shift_strobe_counter
-    (
-        .clk   ( clk                ),
-        .reset ( reset              ),
-        .en    ( shift_strobe       ),
-        .cnt   ( shift_strobe_count )
-    );
-
-    //------------------------------------------------------------------------
-
-    wire out_moore_fsm;
-
-    moore_fsm i_moore_fsm
-    (
-        .clk   ( clk           ),
-        .reset ( reset         ),
-        .en    ( shift_strobe  ),
-        .a     ( out_reg [0]   ),
-        .y     ( out_moore_fsm )
-    );
-
-    wire [7:0] moore_fsm_out_count;
-
-    counter # (8) i_moore_fsm_out_counter
-    (
-        .clk   ( clk                          ),
-        .reset ( reset                        ),
-        .en    ( shift_strobe & out_moore_fsm ),
-        .cnt   ( moore_fsm_out_count          )
-    );
-
-    //------------------------------------------------------------------------
-
-    wire out_mealy_fsm;
-
-    mealy_fsm i_mealy_fsm
-    (
-        .clk   ( clk           ),
-        .reset ( reset         ),
-        .en    ( shift_strobe  ),
-        .a     ( out_reg [0]   ),
-        .y     ( out_mealy_fsm )
-    );
-
-    wire [7:0] mealy_fsm_out_count;
-
-    counter # (8) i_mealy_fsm_out_counter
-    (
-        .clk   ( clk                          ),
-        .reset ( reset                        ),
-        .en    ( shift_strobe & out_mealy_fsm ),
-        .cnt   ( mealy_fsm_out_count          )
-    );
-
-    //------------------------------------------------------------------------
+    // Exercise: Implement FSM for recognizing other sequence,
+    // for example 0101
 
     wire [31:0] number_to_display =
     {
@@ -130,7 +81,7 @@ module top
         moore_fsm_out_count,
         mealy_fsm_out_count
     };
-
+BROKEN
     //------------------------------------------------------------------------
 
     seven_segment_digit i_digit_0 ( number_to_display [ 3: 0], hex0 [6:0]);
